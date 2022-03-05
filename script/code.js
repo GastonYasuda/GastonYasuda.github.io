a9364530cb05e8897a46a52750df1349d524215d
GastonYasuda <gastonyasuda@gmail.com>
2022-03-05 17:41:00 -0300

    incorporacion de js

---
+++ script/code.js
@@ -0,0 +1,16 @@
+const grande = document.querySelector('.grande');
+const boton  = document.querySelectorAll('.boton');
+
+boton.forEach ( (cadaBoton , i) =>{
+    boton[i].addEventListener ('click' , ()=>{
+        let position = i;
+        let operacion = i * (-100/12);
+
+        grande.style.transform = `translateX( ${operacion}% )`;
+        
+        boton.forEach((cadaBoton , i) =>{
+            boton[i].classList.remove('active');
+        })
+        boton[i].classList.add('active');
+    })
+})
